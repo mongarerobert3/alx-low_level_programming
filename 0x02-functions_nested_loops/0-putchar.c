d1a82412b38978f2d50d67e7a0b0fa1ef07dd457
mongarerobert3 <mongarerobert3@gmail.com>
2022-03-16 00:53:09 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,5 +8,16 @@ int main(void)
 {
 	printf("_putchar"\n);
 
-	return (0);
+	return (0)#include <stdio.h>
+
+/* main - prints _putchar, followed by a new line.
+ * return - always 0 (success)
+ */
+
+int main(void)
+{
+        printf("_putchar"\n);
+
+        return (0);
+}
 }
