d01e16faf3005c0a994a8823047c113220be1610
mongarerobert3 <mongarerobert3@gmail.com>
2022-03-16 23:13:15 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch= ‘a’; ch<= ‘z’; ch++)
+
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+return (0);
+}
