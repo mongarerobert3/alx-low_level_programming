60a21bfe46d21525b40ecdb94c50c400bb4d79ef
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-05 13:21:31 -0700

    adhering to betty coding style

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  *_strdup - returns a pointer to a new string which is a duplicate of the string str
  *@str: string to duplicate
  *Return: returns a pointer to the duplicated string
  */
-
 char *_strdup(char *str)
 {
 	char *duplicate_str;
@@ -15,13 +13,10 @@ char *_strdup(char *str)
 
 	if (str == NULL) /* validate str input */
 		return (NULL);
-
 	while (*(str + i))
 		len ++, i++;
 	len++; /*add null terminator to length */
-
 	duplicate_str = malloc(sizeof(char) * len); /* allocates memory */
-
 	if (duplicate_str == NULL) /* validate memory */
 		return (NULL); 
 	i = 0;
