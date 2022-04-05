c0cd90292adae44f8ee2e09127137388432b2d48
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-05 14:10:15 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *free_grid - frees a 2 dimensional grid
+ *previously created by mour alloc_grid function
+ *@grid: ppointer to 2d array
+ *@height: height of array
+ */
+
+void free_grid(int **grid, int hesight)
+{
+	int i;
+	if (grid == NULL)
+		free(grid);
+
+	for ('i = 0; i < height; i++;
+			free(grid[i]);
+
+			free(grid);
+			}
