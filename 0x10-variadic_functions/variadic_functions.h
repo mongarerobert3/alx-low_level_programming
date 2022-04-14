a62d2e8d33d1cb7f337561a3eee02e813959741e
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-14 14:11:15 -0700

    function that prints numbers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,7 @@
-#ifndef VARIDAC_FUNCTIONS
-#define VARIDAC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
 #include <stdarg.h>
-
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -10,7 +9,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
 typedef struct dt
 {
 	char letter;
