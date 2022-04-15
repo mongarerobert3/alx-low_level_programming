e840b23498cdf52fb1977f02947cc28bd744a08d
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-15 14:00:44 -0700

    search for integer in array via function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,10 @@
 #include <stddef.h>
 /**
  *int_index - searches for an integer
+ *array: array
  *@size: the number of elements in teh array
  *@cmp: pointer to the search function
- *@Return - index of where the element is found, if not -1 is the return(not found)
+ *Return - index of where the element is found, if not -1 is the return(not found)
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
