b51a652ade1b22b9c1da87983e8174a5730fab74
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-15 05:31:12 -0700

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
 #include <stdlib.h>
 
 /**
  *print_name - prints a name
  *@name: name of the person
+ *@f: function to execute
  *Return: Nothing
  */
  
 void print_name(char *name, void (*f)(char *))
-{
-	printf("My name is %s\n", name);	
-}
-int main()
 {
 	if (name != NULL && F!= NULL)
 	f(name);
