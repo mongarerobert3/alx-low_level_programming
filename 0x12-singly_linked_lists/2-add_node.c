32ef71329e4aa9a7fe7834cb78ee4e4af00a639a
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-21 12:25:28 -0700

    a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,32 +1,31 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- **add_node - returns the number of elements in a linked list_t list
- *@**head: leading node
- *@*str: linked list
- *return: &new element
- */
+* add_node - add a node to the beginning of a linked_list
+* @head: a pointer to a linked_list
+* @str: string
+* Return: linked_list
+**/
 
 list_t *add_node(list_t **head, const char *str)
 {
-    char *copy_str;
-    list_t *ret_list;
-    unsigned int i;
+	char *copy_str;
+	list_t *ret_list;
+	unsigned int i;
 
-    ret_list = malloc(sizeof*(list_t));
-    if (ret_list == NULL)
-        return NULL;
-    copy_str = strdup(str);
-    for (int i = 0; str[i] != '\0');
-                i += 1;
+	ret_list = malloc(sizeof(list_t));
+	if (ret_list == NULL)
+		return (NULL);
+	copy_str = strdup(str);
+	for (i = 0; str[i] != '\0';)
+		i += 1;
 
-    ret_list -> str = copy_str;
-    ret_list ->next = *head;
-    ret_list ->len = i;
-    *head = ret_list;
-       
-    return (*head);
+	ret_list->str = copy_str;
+	ret_list->next = *head;
+	ret_list->len = i;
+	*head = ret_list;
+	return (*head);
 }
