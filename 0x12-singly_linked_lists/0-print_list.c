989053602043fe72c75b8d81fd0509a06f5d7970
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-21 11:42:14 -0700

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	while ( h != NULL)
 	{
 		if (h -> str == NULL)
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 		count++;
 		h = h -> next;
 	}
-	
-		return (count);
+
+	return (count);
 
 }
