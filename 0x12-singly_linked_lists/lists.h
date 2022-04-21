87ccf7edb50d7bd0142744ff162aada6e0979e43
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-21 11:38:26 -0700

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h> /* size_t, printf */
+#include <stdlib.h> /* malloc */
+#include <string.h> /* strdup */
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
