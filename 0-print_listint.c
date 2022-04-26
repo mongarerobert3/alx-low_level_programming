f4eabf0f915b83f5158d71714a2aa195d76c574b
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-26 14:31:04 +0300

    feeding the files

---
+++ 0-print_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h> /* printf function */
+#include "lists.h"
+
+/**
+ *print_listint - prints all the elements of a listint_t list
+ *@h: list
+ *return: the number of items in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+    size_t element = 0;
+    while( h != NULL)
+    {
+        ++element;
+        printf("%d/n", h ->n); /* autofils 0 if no int value is given */
+    }
+    return (element);
+}
