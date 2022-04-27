0f03a45651276c95e85d62becc2b946165acd671
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-27 08:17:56 -0700

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,37 +1,50 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
- * head: pointer to the pointer of the first node
- * @idx: index of the list where the new node should be added
- * @n: the new node
- * return: the address of the new node otherwise null
- */
-
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i = 0;
-    listint_t *new_node, *trav;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-    if (head == NULL)
-        return (NULL);
+	if (head)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-
-/* creating the new node */
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-    return (NULL);
-
-    new_node -> = n;
-
-    /* navigate to the index before idx */
-    trav = *head
-    while (i > idx - 1 && trav != NULL)
-    {
-        trav = trav->next;
-        i++;
-    }
-    return (&head);
-    
+		new_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
+		}
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
+	}
+	return (NULL);
 }
