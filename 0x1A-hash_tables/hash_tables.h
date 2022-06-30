fc59c8d4e39de1f4bf50732082afc12fbe60dc2a
courte labs <courte_labs@kali.courtewamped_minds>
2022-07-01 00:00:19 +0300

    update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,8 @@
-#ifndef _HASH_TABLES_
-#define _HASH_TABLES_
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -15,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,12 +28,16 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
-/*function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+int add_node(hash_node_t **head, char *key, char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
