04d8aa3e17701aeeacd5181974936fef0576ac79
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-04 12:31:04 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,8 +10,10 @@
 
 int linear_search(int *array, size_t size, int value){
     size_t i = 0;
-    if (array){
-        while (i < size)
+    
+    if (!array)
+        return (-1;)
+    while (i < size)
         {
             printf("Value checked array[%lu] = [%d]\n", i, array[i]);
             if (array[i] == value)
@@ -19,6 +21,5 @@ int linear_search(int *array, size_t size, int value){
 
             i++
         }
-    }
-    return -1
+        return (-1);
 }
